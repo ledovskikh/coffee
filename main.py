e4c5caef44c82f16eb761979dd368b8752f602c2
Anna <annabagr0v@yandex.ru>
2024-03-26 01:48:32 +0300

    Капучино

--- main.py
+++ main.py
@@ -1,7 +1,59 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QTableWidgetItem,
+    QDialog,
+)
 import sqlite3
+
+
+class EditCoffeeForm(QDialog):
+    def __init__(self, coffee_id=None):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.coffee_id = coffee_id
+        self.initUI()
+
+    def initUI(self):
+        self.saveButton.clicked.connect(self.save_coffee)
+
+    def save_coffee(self):
+        name = self.nameEdit.text()
+        roast = self.roastEdit.text()
+        type_ = self.typeComboBox.currentText()
+        description = self.descriptionEdit.toPlainText()
+        price = self.priceEdit.value()
+        volume = self.volumeEdit.value()
+
+        connection = sqlite3.connect("coffee.sqlite")
+        cursor = connection.cursor()
+
+        if self.coffee_id is None:  # Добавление новой записи
+            cursor.execute(
+                """INSERT INTO coffee (name, roast_degree, ground_or_beans, flavor_description, price, volume)
+                              VALUES (?, ?, ?, ?, ?, ?)""",
+                (name, roast, type_, description, price, volume),
+            )
+        else:  # Редактирование существующей записи
+            cursor.execute(
+                """UPDATE coffee SET name=?, roast_degree=?, ground_or_beans=?, flavor_description=?, price=?, volume=?
+                              WHERE id=?""",
+                (
+                    name,
+                    roast,
+                    type_,
+                    description,
+                    price,
+                    volume,
+                    self.coffee_id,
+                ),
+            )
+
+        connection.commit()
+        connection.close()
+        self.accept()
 
 
 class CoffeeApp(QMainWindow):
@@ -9,6 +61,8 @@ class CoffeeApp(QMainWindow):
         super().__init__()
         uic.loadUi("main.ui", self)
         self.load_coffee_data()
+        self.addButton.clicked.connect(self.add_coffee)
+        self.editButton.clicked.connect(self.edit_coffee)
 
     def load_coffee_data(self):
         connection = sqlite3.connect("coffee.sqlite")
@@ -26,6 +80,19 @@ class CoffeeApp(QMainWindow):
                 self.tableWidget.setItem(
                     row_index, column_index, QTableWidgetItem(str(cell_data))
                 )
+
+    def add_coffee(self):
+        editor = EditCoffeeForm()
+        if editor.exec_():
+            self.load_coffee_data()
+
+    def edit_coffee(self):
+        selected = self.tableWidget.currentRow()
+        if selected >= 0:
+            coffee_id = self.tableWidget.item(selected, 0).text()
+            editor = EditCoffeeForm(coffee_id)
+            if editor.exec_():
+                self.load_coffee_data()
 
 
 if __name__ == "__main__":
